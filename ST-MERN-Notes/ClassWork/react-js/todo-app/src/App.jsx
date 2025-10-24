4d21537008775cb1c44aa57ff63f4a1df5929f05
Starboy7704 <saipraneethmokalla@gmail.com>
2025-10-24 13:48:13 +0530

    useState

--- ST-MERN-Notes/ClassWork/react-js/todo-app/src/App.jsx
+++ ST-MERN-Notes/ClassWork/react-js/todo-app/src/App.jsx
@@ -1,11 +1,29 @@
-import Title from "./Title";
+import { useState } from "react";
 
 function App(){
+  const [setUp,setSetUp] = useState("No Joke");
+  const [punchLine,setPunchLine] = useState("No PunchLine");
+
+  async function fetchJoke(){
+    try {
+      const response = await fetch("https://official-joke-api.appspot.com/random_joke");
+      const data = await response.json();
+      console.log(data);
+      console.log(data.setup);
+      setSetUp((previousState)=>(previousState = data.setup))
+      setPunchLine(data.punchline);
+
+
+    } catch (error) {
+      console.error(error);
+    }
+  }
   return(
-    <>
-    <h1>sai praneeth</h1>
-    <Title/>
-    </>
+    <div className="p-12">
+      <button onClick={fetchJoke} className="bg-red-500 hover:bg-red-600 cursor-pointer px-4 py-2 text-white font-semibold rounded-xl">Get a Joke</button>
+      <p className="text-red-600 font-semibold text-lg">{setUp}</p>
+      <p>{punchLine}</p>
+    </div>
   )
 }
 
